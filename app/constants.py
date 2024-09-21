8c9da7111bc89437c3732bbdbf0a5a9cb46f315f
Matei <matei.gardea@gmail.com>
2024-09-21 14:50:09 -0700

    exist

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import os
 
 ASSIGNMENTS = [f"Homework {i}" for i in range(1, 11)] + [
     f"Project {i}" for i in range(1, 6)
@@ -8,11 +9,13 @@ LOCATIONS = ["Online", "Inside Soda 341B", "Outside Soda 341B"]
 ZOOM_LINK = "http://google.com"
 
 users = {}
-with open("data/users.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        users[row[0]] = {
-            "email": row[0].strip().lower(),
-            "name": row[1].strip(),
-            "role": row[2].strip().lower(),
-        }
+os.makedirs("data", exist_ok=True)
+if os.path.exists("data/users.json"):
+    with open("data/users.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            users[row[0]] = {
+                "email": row[0].strip().lower(),
+                "name": row[1].strip(),
+                "role": row[2].strip().lower(),
+            }
