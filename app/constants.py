159742e25371969207d92eec691e7db858720299
Matei <matei.gardea@gmail.com>
2024-10-07 03:59:12 -0700

    constants

--- app/constants.py
+++ app/constants.py
@@ -6,6 +6,6 @@ ASSIGNMENTS = [f"Homework {i}" for i in range(1, 11)] + [
     f"Project {i}" for i in range(1, 6)
 ]
 LOCATIONS = ["Online", "Inside Soda 341B", "Outside Soda 341B"]
-ZOOM_LINK = "http://google.com"
+ZOOM_LINK = "https://berkeley.zoom.us/j/97522023870"
 
 ROSTER_PATH = "data/CS188_Fall_2024_roster.csv"
