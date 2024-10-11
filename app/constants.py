69dec43e302eb7b779215c8e313197e3b3d06ece
Matei <matei.gardea@gmail.com>
2024-10-11 03:04:12 -0700

    inside by default

--- app/constants.py
+++ app/constants.py
@@ -5,7 +5,7 @@ import os
 ASSIGNMENTS = [f"Homework {i}" for i in range(1, 11)] + [
     f"Project {i}" for i in range(1, 6)
 ]
-LOCATIONS = ["Online", "Inside Soda 341B", "Outside Soda 341B"]
+LOCATIONS = ["Inside Soda 341B", "Outside Soda 341B", "Online"]
 ZOOM_LINK = "https://berkeley.zoom.us/j/97522023870"
 
 ROSTER_PATH = "data/CS188_Fall_2024_roster.csv"
