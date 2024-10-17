e442c2c02100cd87d41309d474e3dbe9fcaecd2c
Matei <matei.gardea@gmail.com>
2024-10-16 17:08:39 -0700

    favicon and other assignment option

--- app/constants.py
+++ app/constants.py
@@ -2,9 +2,11 @@ import csv
 import json
 import os
 
-ASSIGNMENTS = [f"Homework {i}" for i in range(1, 11)] + [
-    f"Project {i}" for i in range(1, 6)
-]
+ASSIGNMENTS = (
+    [f"Homework {i}" for i in range(1, 11)]
+    + [f"Project {i}" for i in range(1, 6)]
+    + ["Other"]
+)
 LOCATIONS = ["Inside Soda 341B", "Outside Soda 341B", "Online"]
 ZOOM_LINK = "https://berkeley.zoom.us/j/97522023870"
 
